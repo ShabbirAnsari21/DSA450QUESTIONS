a60ba69626e555a9e4143e3bd1afff912268c1f8
Shabbir Ansari <sansari@ariqt.com>
2022-08-29 11:37:22 +0530

    First reverse array

---
+++ ReverseArray/ReverseArray/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace ReverseArray
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            int[] array = { 1, 2, 3, 4, 5, 6, 7, 8 };
+            PrintArray(array);
+            Console.WriteLine("Array after reverse");
+            ReverseArray(array);
+            PrintArray(array);
+        }
+
+        public static void PrintArray(int[] array)
+        {
+            for(int i = 0; i < array.Length; i++)
+            {
+                Console.Write(array[i] + " ");
+            }
+            Console.WriteLine();
+        }
+        public static void ReverseArray(int[] array)
+        {
+            int end = array.Length - 1;
+            int start = 0;
+            int temp;
+            while(start < end)
+            {
+                temp = array[start];
+                array[start] = array[end];
+                array[end] = temp;
+                start++;
+                end--;
+            }
+        }
+    }
+}
